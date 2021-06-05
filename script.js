2682307c73130027a0bbd9747515a5bc996cb3b0
Irshad55 <syedirshadmdy@gmail.com>
2021-06-05 21:32:11 +0530

    Making notes

--- script.js
+++ script.js
@@ -1,44 +1,141 @@
-console.log("loading JS")
+// console.log("loading JS")
 
-var a = "3"
-var b = 3
-var c
-var d = null
-var e = true
-var a = "3";
-console.log(a, typeof(a));
+// alert("HELLO WORLD")
 
-var b = 3;
-console.log(b, typeof(b));
+// var a = "3"
+// var b = 3
+// var c
+// var d = null
+// var e = true
+// var a = "3";
+// console.log(a, typeof(a));
 
-var c;
-console.log(c, typeof(c));
+// var b = 3;
+// console.log(b, typeof(b));
 
-var d = null;
-console.log(d, typeof(d));
+// var c;
+// console.log(c, typeof(c));
 
-var e = true;
-console.log(e, typeof(e));
+// var d = null;
+// console.log(d, typeof(d));
 
-// hosting
-console.log(g, typeof(g));
-var g = 6
+// var e = false;
+// console.log(e, typeof(e));
 
-// if
+// // hosting
+// console.log(g, typeof(g));
+// var g = 6
 
-a = 3, b = 5
+// // if
 
-if (a > b) {
-    console.log(a, "is greater")
+
+// a = 3, b = 5
+
+// if (a > b) {
+//     console.log(a, "is greater")
+// }
+// else {
+//     console.log(b, "is greater")
+// }
+
+// // function
+
+// function add(a, b) {
+//     var sum = a + b;
+//     console.log("Total count is", sum)
+// }
+// add(55, 6);
+
+// function odd_even(a) {
+//     if (a % 2 == 0) {
+//     console.log(a, "Even number")
+//     }
+//     else {
+//         console.log(a, "odd number")
+//     }
+// }
+// odd_even(5)
+
+// function FizzBuzz() {
+//     var a;
+//     var b;
+//     if (a / 3 == 0 ) {
+//         console.log(a, "Fizz")
+//     }
+//     else {
+//         console.log(a)
+//     }
+// }
+
+// var language = ["English", "Hindi", "Kannada", 5,6,7,8 ]
+// console.log(language[0])
+
+// function voting(age) {
+//     if (age > 18) {
+//         console.log(age, "Eligible for voting")
+//     } else {
+//         console.log(age, "Not Eligible for Voting")
+//     }
+// }
+// voting(20);
+
+// function school(grade) {
+//     if ( grade <= 7 ) {
+//     console.log( "Primary school")
+// } else if ( grade >= 8 && grade <= 10) {
+//     console.log("High School")
+// } else {
+//     console.log("Firstgrade")
+// }
+// }
+// school(11)
+
+// if(condition) {
+//     // statement
+// }else {
+//     //default statement
+// }
+
+// if(condition) {
+//     // statement
+// } else if (condition) {
+//     //statement
+// } else {
+//     //default statement
+// }
+
+// // for loop
+
+// for(initial Expression;condition;increment /decrement expression) {
+
+// }
+
+
+
+function fizz_buzz(star) {
+    if ((star % 15) == 0 ) {
+    console.log("FizzBuzz")
+    }else if ((star % 3) == 0 ) {
+        console.log("Fizz")
+    }else if ((star % 5 ) == 0 ) {
+        console.log("Buzz")
+    }else {
+        console.log("No issues")
+    }
 }
-else {
-    console.log(b, "is greater")
-}
+fizz_buzz(0)
 
-// function
 
-function add(a, b) {
-    var sum = a + b;
-    console.log("Total count is", sum)
-}
-add(55, 6);
+
+
+// var fruits = ['Apple', 'Mango', 'Grapes', 'Guava', 'berry']
+ 
+// console.log(fruits)
+// console.log(fruits.splice(0,2))
+// console.log(fruits)
+
+// console.log(fruits.splice(1,0,'cherry'))
+// console.log(fruits)
+
+// console.log(fruits.shift())
+// console.log(fruits)
